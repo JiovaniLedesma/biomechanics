55372ac1e994d4a44509751ceb4644440fae5f28
Jiovani Ledesma Arredondo <jiovaniledesmaa@gmail.com>
2019-05-26 12:47:44 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+# Current status: pre-alpha
+
+setup(name='biomechanics',
+      version='0.1.0',
+      description='Python library for knowlegde about biomechanics',
+      author='Jiovani Ledesma & Israel Miguel',
+      author_email='jiovaniledesmaa@gmail.com',
+      license = "MIT",
+      keywords=["biomechanics", "kinematics and dynamics analysis", "March"],
+      url='https://github.com/JiovaniLedesma/biomechanics',
+      packages=['biomechanics'],
+      classifiers=[
+      "Development Status :: 2 - Pre-Alpha",
+      "Intended Audience :: Education",
+      "Intended Audience :: Developers",
+      "Intended Audience :: Reseachers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3.6",
+      "Programming Language :: Python :: Implementation :: CPython",
+      ]
+      )
